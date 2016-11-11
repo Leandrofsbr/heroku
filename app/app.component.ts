1254f9761922265019b0664fca3101fa93dec028
Leandro Souza <leandrofsbr@gmail.com>
2016-11-11 09:22:23 -0200

    Revert "commit inicial"
    This reverts commit e65e40db231d844c3234c6a16883145fb507d862.

--- app/app.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'my-app',
-  template: '<div class="container"><div class="alert alert-danger" role="alert"> <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span> <span class="sr-only">Error:</span> Enter a valid email address</div></div>'
-})
-export class AppComponent { }
