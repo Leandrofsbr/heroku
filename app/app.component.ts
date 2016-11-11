e65e40db231d844c3234c6a16883145fb507d862
Leandro Souza <leandrofsbr@gmail.com>
2016-11-11 09:17:19 -0200

    commit inicial

---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<div class="container"><div class="alert alert-danger" role="alert"> <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span> <span class="sr-only">Error:</span> Enter a valid email address</div></div>'
+})
+export class AppComponent { }
